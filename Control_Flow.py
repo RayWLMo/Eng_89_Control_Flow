25777648894311e8dcad1343b2b1f1d36bc52f54
RayWLMo <Raymond.WL.Mo@gmail.com>
2021-06-23 11:45:13 +0100

    updated README.md and Control_Flow.py files

--- Control_Flow.py
+++ Control_Flow.py
@@ -1,27 +1,19 @@
-# Python Control Flow with if, elif and else conditions
+# Syntax - if variable -condition age:
 
-- Syntax - `if variable -condition:`
-```Python
 weather = "sunny"
 
 if weather == "sunny":  # If this condition is true, the next line would execute
-    print("Enjoy the weather!")
+    print("Enjoy the weather!")  # This line needs to be inside the if code block - indented
 
 elif weather != "sunny":
     print("Might need to bring an umbrella")
 else:
     print("Have a nice day!")
-```
-- Ensure the contents of the condition is inside the if code block -  it needs to be indented
+# Lets create a ticket age criteria
 
-###Lets create a ticket age criteria
-```Python
 age = 16
 
 if age <18:  # Checking age provided
     print("You are not eligible to watch this movie, please choose an age appropriate movie")
 else:
     print("Enjoy the movie!")
-```
-
-# Loops - for loops and while loops
